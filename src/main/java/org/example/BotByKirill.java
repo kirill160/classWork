9f70bf8d3724667578d548a9fb9247144b47ae17
kirill <kirillstan1997@gmail.com>
2023-01-01 22:58:04 +0300

    my bot

---
+++ src/main/java/org/example/BotByKirill.java
@@ -0,0 +1,23 @@
+package org.example;
+
+import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.meta.api.objects.Update;
+
+public class BotByKirill extends TelegramLongPollingBot {
+
+    @Override
+    public String getBotUsername() {
+        return  "simple";
+    }
+
+    @Override
+    public String getBotToken() {
+        return "5663600364:AAGlpkMKeCM7-4h07PbWaVQn3bTxqQTYoWU";
+    }
+
+    @Override
+    public void onUpdateReceived(Update update) {
+        System.out.println(update.getMessage().getText());
+        System.out.println(update.getMessage().getFrom().getFirstName());
+    }
+}
